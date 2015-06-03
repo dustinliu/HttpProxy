80be72268e27db3dfc79e5d38bd7d4b81feefc12
Dustin Liu <dustinl@yahoo-inc.com>
2015-06-03 15:16:52 +0800

    checkstyle fix

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # HttpProxy
 simple java http proxy
-=======
+
+## Todo
+- implement http method connect for https
+- cache the ChannelHandler class
--- src/main/java/dustinl/proxy/config/PluginConfig.java
+++ src/main/java/dustinl/proxy/config/PluginConfig.java
@@ -1,19 +1,40 @@
 package dustinl.proxy.config;
 
+/**
+ * The type Plugin config.
+ */
 public class PluginConfig {
 
+    /** The plugin name. */
     private String name;
+    /** The class name. */
     private String className;
 
+    /**
+     * Instantiates a new Plugin config.
+     *
+     * @param name the plugin name
+     * @param className the class name
+     */
     public PluginConfig(String name, String className) {
         this.name = name;
         this.className = className;
     }
 
+    /**
+     * Gets the plugin name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets plugin class name.
+     *
+     * @return the class name
+     */
     public String getClassName() {
         return className;
     }
